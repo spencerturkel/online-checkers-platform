32d8339897b78d74ced54e3509eea7c61128c99e
Eddiebadeddie <eduardn@mail.usf.edu>
2018-10-07 20:47:35 -0400

    Moved everything from index to app

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,22 @@
 <template>
+  <link rel="stylesheet" href="index.css">
+  <title>Checkers</title>
+
+  <a href="https://google.com">
+    <button id="START">
+      START
+    </button>
+  </a>
+
+  <a href="https://google.com">
+    <button id="ACCOUNT">
+      ACCOUNT
+    </button>
+  </a>
+
+  <img id = "StartScreen" src="../src/assets/CheckersMainMenuGraphic.png" alt= "Start Screen" width="50%" height = "50%">
+
+<!--
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
@@ -7,8 +25,9 @@
     <router-view/>
   </div>
 </template>
-
+-->
 <style>
+/*
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,5 +46,41 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}*/
+
+/*Allows for Full Scale background*/
+body, html{
+  height : 100%;
+}
+
+/*Accesses the Start Screen*/
+#StartScreen{
+  height : 100%;
+
+  /*Centers the image and does not allow it to repeat*/
+  background: no-repeat center;
+  background-size: cover;
+
+  /*Centers the image along the horizontal*/
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+/*Not entirely sure what this does, but it's necessary*/
+button{
+  position:absolute;
+}
+
+/*Sets the Start Button's Position*/
+#START{
+  top:15%;
+  left: 50%;
+}
+
+/*Sets the Account Button's Position*/
+#ACCOUNT{
+  top:20%;
+  left: 48.6%;
 }
 </style>
